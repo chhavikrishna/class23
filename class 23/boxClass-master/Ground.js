463c0d49973e004b3336f7bff735277695b32a03
chhavikrishna <84122164+chhavikrishna@users.noreply.github.com>
2021-06-16 13:01:54 +0530

    Add files via upload

---
+++ class 23/boxClass-master/Ground.js
@@ -0,0 +1,19 @@
+class Ground
+{
+    constructor(x,y,width,height){
+        var gopt={
+            isStatic :true
+
+        }
+        this.body=Bodies.rectangle(x,y,width,height,gopt);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body)
+    }
+    display(){
+        var p=this.body.position;
+        rectMode(CENTER);
+        fill ("brown")
+        rect (p.x,p.y,this.width,this.height)
+    }
+}
